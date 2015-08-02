d28de5361b16bbee85ebd2244a63211c4b6dd209
[MStommes] <mstommes@gmail.com>
2015-08-02 16:59:57 -0500

    this is the 2nd push; the carousel now contains a a load a slide up and a slide down button with the rate of the slide

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,41 +1,38 @@
 $(document).ready(function(){
     var personCounter = 0;
 
-    $("#buttons").append("<button id='back' class='btn btn-group-lg'>Back </button>");
-    $("#buttons").append("<button id='forward' class='btn btn-group-lg'>Forward</button>");
+    $("#buttons").append("<button id='load' class='btn btn-group-lg'>Load Carousel</button>");
+    $("#buttons").append("<button id='up' class='btn btn-group-lg'>Click To Slide Carousel Up</button>");
+    $("#buttons").append("<button id='down' class='btn btn-group-lg'>Click To Slide Carousel Down</button>");
 
-    function addNextClassmate (index, people) {
-        for (var i = 0; i < people.length; i++) {
-            if (i == personCounter) {
-                initialCall();
+    var initialCall = $("#buttons").on('click', '#load', function () {
+        $.ajax({
+            url: "/data",
+            success: function (data) {
+                console.log(data);
+                $.each(data, function (index, people) {
+                    var newDiv = $("#classmatecard").last();
+                    newDiv.append("<p class='well'><strong>Classmate Name : </strong>" + people.name + "</br>" + "<strong>Summary Statement:</strong> " + people.desc + "</br>" + "<strong>Kudos:</strong>" + people.thanks + "</p>");
+
+                });
             }
-        }
-    }
+        });
+    });
 
-     var initialCall = $("#buttons").on('click', '#forward', function () {
-            $.ajax({
-                url: "/data",
-                success: function (data) {
-                    console.log(data);
-                    $.each(data, function (index, people) {
-                        var newDiv = $("#classmatecard").last();
-                        newDiv.append("<p class='well'>Classmate Name : " + people.name + "</p>");
-                        newDiv.append("<p class='well'>Summary Statement:  " + people.desc + "</p>");
-                        newDiv.append("<p class='well'>Shoutout:  " + people.thanks + "</p>");
+    var setSlide = true;
 
-
-                    });
-                },
-                complete: function(data){
-                    addNextClassmate(index, people);
-                }
-            });
-     });
+    $("#buttons").on('click', '#up', function(){
+      if(setSlide) {
+          $("#classmatecard").slideUp(3500);
+          setSlide = false;
+      }else {
+          $("#buttons").on('click', '#down', function(){
+              $("#classmatecard").slideDown(3500);
+          setSlide = true;
+      });
+      }
+    });
 
 });
-
-
-
-
 
 
