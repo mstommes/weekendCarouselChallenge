3da1364bb9d64dae3356eb31554ef8a879b68638
[MStommes] <mstommes@gmail.com>
2015-10-03 06:12:18 -0500

    updating

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,43 @@
 $(document).ready(function(){
-    var personCounter = 0;
-
-    $("#buttons").append("<button id='load' class='btn btn-group-lg'>Load Carousel</button>");
-    $("#buttons").append("<button id='up' class='btn btn-group-lg'>Click To Slide Carousel Up</button>");
-    $("#buttons").append("<button id='down' class='btn btn-group-lg'>Click To Slide Carousel Down</button>");
-
-    var initialCall = $("#buttons").on('click', '#load', function () {
-        $.ajax({
-            url: "/data",
-            success: function (data) {
-                console.log(data);
-                $.each(data, function (index, people) {
-                    var newDiv = $("#classmatecard").last();
-                    newDiv.append("<p class='well'><strong>Classmate Name : </strong>" + people.name + "</br>" + "<strong>Summary Statement:</strong> " + people.desc + "</br>" + "<strong>Kudos:</strong>" + people.thanks + "</p>");
-                    return false;
-                });
-            }
-        });
+    $.ajax({
+        url: "/data",
+        success: function (data) {
+            console.log(data);
+            $.each(data, function (index,people) {
+                console.log (index);
+                var newDiv = $(".classcontainer").last();
+                newDiv.append("<p class='well classmatecard'><strong>Classmate Name : </strong>" + people.name + "</br>" + "<strong>Summary Statement:</strong> " + people.desc + "</br>" + "<strong>Kudos:</strong>" + people.thanks + "</p>");
+            });
+            $(".classmatecard").first().addClass("active");
+        }
     });
 
-    var setSlide = true;
 
-    $("#buttons").on('click', '#up', function(){
-      if(setSlide) {
-          $("#classmatecard").slideUp(3500);
-          setSlide = false;
-      }else {
-          $("#buttons").on('click', '#down', function(){
-              $("#classmatecard").slideDown(3500);
-          setSlide = true;
-      });
-      }
+    $("#buttons").append("<button id='previous' class='btn btn-group-lg'>previous</button>");
+    $("#buttons").append("<button id='next' class='btn btn-group-lg'>next</button>");
+
+     $("#buttons").on('click', '#next', function () {
+        var currentClass = $(".active");
+         var nextClass = currentClass.next();
+         if (nextClass.length ===0){
+             nextClass = $('.classmatecard').first();
+         }
+         currentClass.removeClass("active");
+         nextClass.addClass("active");
     });
+
+
+    $("#buttons").on('click', '#previous', function () {
+        var currentClass = $(".active");
+        var previousClass = currentClass.prev();
+        if (previousClass.length ===0){
+            previousClass = $('.classmatecard').last();
+        }
+        currentClass.removeClass("active");
+        previousClass.addClass("active");
+    });
+
+
 
 });
 
